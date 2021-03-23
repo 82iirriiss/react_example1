a4e2fd4afb5559947f34f61fe2a63f010193e4f2
kimyunmi <82iirriiss@daum.net>
2021-03-24 00:11:46 +0900

    react-route-dom 실습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import { Route,Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr/>
+      <Route path='/' component={Home} exact={true}/>
+      <Route path={['/about', '/info']} component={About}/>
+      {/* <Route path="/info" component={About}/> */}
+      
     </div>
   );
-}
+};
 
 export default App;
